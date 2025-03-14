31dbbd76d9940d9bd2139039efb8ee7fb13117ec
Luchman <lakshmanvijay28@gmail.com>
2025-03-14 23:48:02 +0530

    added images

--- src/assets/image.js
+++ src/assets/image.js
@@ -1,6 +1,5 @@
 import sweetcornSoup from "./sweetcorn_soup,_creamy_and_steaming,_gar.png";
 import mushroomSoup from "./creamy_mushroom_soup,_garnished_with_sau.png";
-
 import tomatoSoup from "./tomato_soup,_vibrant_red,_topped_with_fr.png";
 import frenchOnionSoup from "./French_onion_soup,_topped_with_melted_ch.png";
 import vegClearSoup from "./clear_vegetable_soup,_with_diced_carrots.png";
@@ -37,47 +36,146 @@ import DalFry from "./Dal_Fry,_a_pop.png";
 import DalTadka from "./Dal_Tadka,_a_c.png";
 import PalakPaneer from "./Palak_Paneer,_.png";
 import DalMakani from "./Dal_Makani,_a_.png";
-import MalaiKofta from "./Malai_Kofta,_a.png"
+import MalaiKofta from "./Malai_Kofta,_a.png";
+import achariPaneer from "./assets/Achari_Paneer.png";
+import hariyaliPaneer from "./assets/Hariyali_Paneer.png";
+import malaiPaneer from "./assets/Malai_Paneer.png";
+import mushroomTikka from "./assets/Mushroom_Tikka.png";
+import paneerSheek from "./assets/Paneer_Sheek.png";
+import paneerTikka from "./assets/Paneer_Tikka.png";
+import vegSheekKebab from "./assets/Veg_Sheek_Kebab.png";
+import GobiFriedRice from "./fried_rice/Gobi_Fried_Rice.png";
+import MixedFriedRice from "./fried_rice/Mixed_Fried_Rice.png";
+import MixedSCZFriedRice from "./fried_rice/Mixed_SCZ_Fried_Rice.png";
+import MushroomFriedRice from "./fried_rice/Mushroom_Fried_Rice.png";
+import PaneerFriedRice from "./fried_rice/Paneer_Fried_Rice.png";
+import SCZGobiFriedRice from "./fried_rice/SCZ_Gobi_Fried_Rice.png";
+import SCZMushroomFriedRice from "./fried_rice/SCZ_Mushroom_Fried_Rice.png";
+import SCZPaneerFriedRice from "./fried_rice/SCZ_Paneer_Fried_Rice.png";
+import SCZVegFriedRice from "./fried_rice/SCZ_Veg_Fried_Rice.png";
+import VegFriedRice from "./fried_rice/Veg_Fried_Rice.png";
+import GulabJamun from "./desserts_images/Gulab_Jamun.png";
+import Rasgulla from "./desserts_images/Rasgulla.png";
+import Rasmalai from "./desserts_images/Rasmalai.png";
+import GajarKaHalwa from "./desserts_images/Gajar_Ka_Halwa.png";
+import Kheer from "./desserts_images/Kheer.png";
+import ChocolateBrownie from "./desserts_images/Chocolate_Brownie.png";
+import IceCream from "./desserts_images/Ice_Cream.png";
+import Lassi from "./drinks/A_tall_glass_of_Lassi,_a_traditional_Indian_yogurt.png";
+import SweetLassi from "./drinks/A_glass_of_Sweet_Lassi,_a_traditional_Indian_yogur.png";
+import SaltedLassi from "./drinks/A_glass_of_Salted_Lassi,_a_traditional_Indian_yogu.png";
+import ColdCoffee from "./drinks/A_tall_glass_of_Cold_Coffee,_rich_and_creamy,_topp.png";
+import MasalaChai from "./drinks/A_cup_of_Masala_Chai,_a_traditional_Indian_spiced_.png";
+import GreenTea from "./drinks/A_cup_of_Green_Tea,_light_golden-green_in_color,_s.png";
+import BlackCoffee from "./drinks/A_cup_of_Black_Coffee,_dark_and_rich,_served_in_a_.png";
+import SoftDrinks from "./drinks/A_glass_of_Soft_Drinks,_fizzy_and_refreshing,_fill.png";
+import MineralWater from "./drinks/A_bottle_of_Mineral_Water,_crystal_clear,_with_a_b.png";
+import AlooParatha from "../assets/breads/A_realistic_image_of_Aloo_Paratha_an_Indian_stuff.png";
+import ButterKulcha from "../assets/breads/A_realistic_image_of_Butter_Kulcha_a_soft_and_flu.png";
+import ButterNaan from "../assets/breads/A_realistic_image_of_Butter_Naan_a_soft_Indian_fl.png";
+import ButterPulka from "../assets/breads/A_realistic_image_of_Butter_Pulka_a_soft_and_roun.png";
+import CheeseNaan from "../assets/breads/A_realistic_image_of_Cheese_Naan_a_soft_Indian_fl.png";
+import ChilliCheeseNaan from "../assets/breads/A_realistic_image_of_Chilli_Cheese_Naan_a_soft_ln.png";
+import ChilliGarlicNaan from "../assets/breads/A_realistic_image_of_Chilli_Garlic_Naan_a_soft_ln.png";
+import ChilliNaan from "../assets/breads/A_realistic_image_of_Chilli_Naan_a_soft_Indian_fl.png";
+import GarlicNaan from "../assets/breads/A_realistic_image_of_Garlic_Naan_a_soft_Indian_fl.png";
+import Kulcha from "../assets/breads/A_realistic_image_of_Kulcha_a_soft_and_fluffy_Ind.png";
+import LachaParatha from "../assets/breads/A_realistic_image_of_Lacha_Paratha_a_flaky_and_la.png";
+import MethiParatha from "../assets/breads/A_realistic_image_of_Methi_Paratha_an_Indian_flat.png";
+import Naan from "../assets/breads/A_realistic_image_of_Naan_a_soft_and_fluffy_India.png";
+import Pulka from "../assets/breads/A_realistic_image_of_Pulka_a_soft_and_round_India.png";
+import TandooriRoti from "../assets/breads/A_realistic_image_of_Tandoori_Roti_an_Indian_whol.png";
+
+
+
 
 let imagelist = [
-  sweetcornSoup,
-  mushroomSoup,
-  tomatoSoup,
-  frenchOnionSoup,
-  vegClearSoup,
-  vegManchowSoup,
-  hotSourSoup,
-  lemonCorianderSoup,
-  gobiManchurian,
-  mushroomManchurian,
-  paneerManchurian,
-  babyCornManchurian,
-  chilliPaneer,
-  chilliGobi,
-  chilliBabyCorn,
-  gobiPepperFry,
-  paneerPepperFry,
-  mushroomPepperFry,
-  dragonPaneer,
-  dragonGobi,
-  chilliMushroom,
-  chinesePlatter,
-  GobiMasala,
-  MushroomMasala,
-  PaneerButterMasala,
-  BabyCornMasala,
-  KadaiPaneer,
-  PaneerTikkaMasala,
-  ShahiPaneer,
-  MixVegGravy,
-  KadaiVeg,
-  MalaiKofta,
-  AlooGobiMasala,
-  GreenPeasMasala,
-  KadaiMushroom,
-  DalFry,
-  DalTadka,
-  PalakPaneer,
-  DalMakani,
+  { cat: "Soups", img: sweetcornSoup },
+  { cat: "Soups", img: mushroomSoup },
+  { cat: "Soups", img: tomatoSoup },
+  { cat: "Soups", img: frenchOnionSoup },
+  { cat: "Soups", img: vegClearSoup },
+  { cat: "Soups", img: vegManchowSoup },
+  { cat: "Soups", img: hotSourSoup },
+  { cat: "Soups", img: lemonCorianderSoup },
+  { cat: "Starters", img: gobiManchurian },
+  { cat: "Starters", img: mushroomManchurian },
+  { cat: "Starters", img: paneerManchurian },
+  { cat: "Starters", img: babyCornManchurian },
+  { cat: "Starters", img: chilliPaneer },
+  { cat: "Starters", img: chilliGobi },
+  { cat: "Starters", img: chilliBabyCorn },
+  { cat: "Starters", img: gobiPepperFry },
+  { cat: "Starters", img: paneerPepperFry },
+  { cat: "Starters", img: mushroomPepperFry },
+  { cat: "Starters", img: dragonPaneer },
+  { cat: "Starters", img: dragonGobi },
+  { cat: "Starters", img: chilliMushroom },
+  { cat: "Starters", img: chinesePlatter },
+  { cat: "Gravies", img: GobiMasala },
+  { cat: "Gravies", img: MushroomMasala },
+  { cat: "Gravies", img: PaneerButterMasala },
+  { cat: "Gravies", img: BabyCornMasala },
+  { cat: "Gravies", img: KadaiPaneer },
+  { cat: "Gravies", img: PaneerTikkaMasala },
+  { cat: "Gravies", img: ShahiPaneer },
+  { cat: "Gravies", img: MixVegGravy },
+  { cat: "Gravies", img: KadaiVeg },
+  { cat: "Gravies", img: MalaiKofta },
+  { cat: "Gravies", img: AlooGobiMasala },
+  { cat: "Gravies", img: GreenPeasMasala },
+  { cat: "Gravies", img: KadaiMushroom },
+  { cat: "Gravies", img: DalFry },
+  { cat: "Gravies", img: DalTadka },
+  { cat: "Gravies", img: PalakPaneer },
+  { cat: "Gravies", img: DalMakani },
+  { cat: "Tandoori Starters", img: achariPaneer },
+  { cat: "Tandoori Starters", img: hariyaliPaneer },
+  { cat: "Tandoori Starters", img: malaiPaneer },
+  { cat: "Tandoori Starters", img: mushroomTikka },
+  { cat: "Tandoori Starters", img: paneerSheek },
+  { cat: "Tandoori Starters", img: paneerTikka },
+  { cat: "Tandoori Starters", img: vegSheekKebab },
+  { cat: "Fried Rice", img: GobiFriedRice },
+  { cat: "Fried Rice", img: MixedFriedRice },
+  { cat: "Fried Rice", img: MixedSCZFriedRice },
+  { cat: "Fried Rice", img: MushroomFriedRice },
+  { cat: "Fried Rice", img: PaneerFriedRice },
+  { cat: "Fried Rice", img: SCZGobiFriedRice },
+  { cat: "Fried Rice", img: SCZMushroomFriedRice },
+  { cat: "Fried Rice", img: SCZPaneerFriedRice },
+  { cat: "Fried Rice", img: SCZVegFriedRice },
+  { cat: "Fried Rice", img: VegFriedRice },
+  { cat: "Desserts", img: GulabJamun },
+  { cat: "Desserts", img: Rasgulla },
+  { cat: "Desserts", img: Rasmalai },
+  { cat: "Desserts", img: GajarKaHalwa },
+  { cat: "Desserts", img: Kheer },
+  { cat: "Desserts", img: ChocolateBrownie },
+  { cat: "Desserts", img: IceCream },
+  { cat: "Beverages", img: Lassi },
+  { cat: "Beverages", img: SweetLassi },
+  { cat: "Beverages", img: SaltedLassi },
+  { cat: "Beverages", img: ColdCoffee },
+  { cat: "Beverages", img: MasalaChai },
+  { cat: "Beverages", img: GreenTea },
+  { cat: "Beverages", img: BlackCoffee },
+  { cat: "Beverages", img: SoftDrinks },
+  { cat: "Beverages", img: MineralWater },
+  { cat: "Indian Breads", img: AlooParatha },
+  { cat: "Indian Breads", img: ButterKulcha },
+  { cat: "Indian Breads", img: ButterNaan },
+  { cat: "Indian Breads", img: ButterPulka },
+  { cat: "Indian Breads", img: CheeseNaan },
+  { cat: "Indian Breads", img: ChilliCheeseNaan },
+  { cat: "Indian Breads",  img: ChilliGarlicNaan },
+  { cat: "Indian Breads",  img: ChilliNaan },
+  { cat: "Indian Breads",  img: GarlicNaan },
+  { cat: "Indian Breads",  img: Kulcha },
+  { cat: "Indian Breads",  img: LachaParatha },
+  { cat: "Indian Breads",  img: MethiParatha },
+  { cat: "Indian Breads",  img: Naan },
+  { cat: "Indian Breads",  img: Pulka },
+  { cat: "Indian Breads",  img: TandooriRoti }
 ];
 export { imagelist };
